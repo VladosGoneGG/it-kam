f9101d8a23f2cc59005c3b57ed63a72c09918652
Vladislav Medelean <vlad93gm@gmail.com>
2023-11-11 20:59:32 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
 				<div className='app-wrapper-content'>
 					<Routes>
 						<Route path='/profile' element={<Profile />} />
+						<Route path='/dialogs' element={<Dialogs />} />
 					</Routes>
 				</div>
 			</div>
