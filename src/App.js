ff853183d1bcf8c6f3640c64c6e14b7d9d3468f9
Vladislav Medelean <vlad93gm@gmail.com>
2023-11-11 19:54:15 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Header } from "./Components/Header/Header";
+import {Header} from "./Components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+        </div>
+    );
 }
 
 export default App;
